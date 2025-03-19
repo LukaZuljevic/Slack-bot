59bf90328ba305ec947250d03805ae1c9fcd8e08
zulja1 <luka.zuljo@gmail.com>
2025-03-19 17:44:14 +0100

    Setup the slack bot to work

---
+++ app.js
@@ -0,0 +1,20 @@
+const { App } = require("@slack/bolt");
+require("dotenv").config();
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+});
+
+app.message("hello", async ({ message, say }) => {
+  console.log("message");
+  await say(`Hey there <@${message.user}>!`);
+});
+
+(async () => {
+  await app.start();
+
+  app.logger.info("⚡️ Bolt app is running!");
+})();
